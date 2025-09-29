d653efb534014d7421d139dbf7be04a99d3682bd
Dragos Tudorache <hedphug@gmail.com>
2025-09-29 13:41:07 +0300

    Remove old files; keep only new monorepo structure

--- src/app/command-interpreter/IntentCommand.ts
+++
@@ -1,9 +0,0 @@
-import { WitEntities } from './wit-interpreter.service';
-
-export interface IntentCommandConstructor {
-  new (n: WitEntities): IntentCommand;
-}
-
-export interface IntentCommand {
-  description: { [key: string]: any }
-}
