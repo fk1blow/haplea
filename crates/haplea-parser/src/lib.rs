79ede8cfd06ad18a1633f65531875b7e216e4ef0
Dragos Tudorache <hedphug@gmail.com>
2025-10-12 21:28:29 +0300

    add heading token extraction

--- crates/haplea-parser/src/lib.rs
+++ crates/haplea-parser/src/lib.rs
@@ -1,19 +1,18 @@
 mod scanner;
 mod token;
+use crate::scanner::Scanner;
 
 /// Parse a markdown recipe and return structured data
 pub fn parse_recipe(markdown: &str) -> String {
-    print!("{markdown}");
+    let mut scanner = Scanner::new(String::from(markdown));
+    scanner.scan();
+
     "Parsed Recipe Stub".to_string()
 }
 
 #[cfg(test)]
 mod tests {
-
-    use crate::scanner::Scanner;
-
     use super::*;
-    // use token::*;
 
     #[test]
     fn test_parse_recipe() {
@@ -25,7 +24,9 @@ mod tests {
 
         // alternative to inline markdown
         // const MARKDOWN: &str = include_str!("test.md");
-        let markdown_1 = "# ";
+        let markdown_1 = r#"# My first recipe
+
+        "#;
 
         let _markdown_3 = r#"
             # hello world
@@ -48,15 +49,9 @@ mod tests {
             some notes about this recipe
         "#;
 
-        let mut scanner = Scanner::new(String::from(markdown_1));
-        scanner.scan();
-        println!("markdown source: \n {markdown_1} \nmardown end");
-        // format!("scanner source: {:?}", scanner.source);
-        // println!("scanner source: {:?}", scanner.source);
-        // println!("{:?}", scanner.tokens);
+        parse_recipe(markdown_1);
 
-        // let result = parse_recipe(markdown_1);
         // assert_eq!(result, "Parsed Recipe Stub");
-        assert_eq!(true, true);
+        // assert_eq!(true, true);
     }
 }
--- crates/haplea-parser/src/scanner.rs
+++ crates/haplea-parser/src/scanner.rs
@@ -3,6 +3,7 @@ use crate::token::*;
 #[derive(Debug)]
 pub struct Scanner {
     source: String,
+    tokens: Vec<Token>,
     chars: Vec<char>,
     pos: usize,
     line: usize,
@@ -13,6 +14,7 @@ impl Scanner {
         let chars = source.chars().collect();
         Scanner {
             source,
+            tokens: Vec::new(),
             chars,
             pos: 0,
             line: 0,
@@ -23,10 +25,15 @@ impl Scanner {
         self.pos >= self.chars.len()
     }
 
+    fn advance(&mut self) {
+        self.pos += 1
+    }
+
     fn is_heading(&self) -> bool {
         let mut current_pos = self.pos;
         let mut heading_level: usize = 0;
 
+        // first char is not a hash, it's not a heading
         if self.chars[current_pos] != '#' {
             return false;
         }
@@ -48,21 +55,43 @@ impl Scanner {
         true
     }
 
+    fn heading(&mut self) -> String {
+        // TODO can i use something like `can_advance` which would check
+        // if self.pos is less than self.chars.len() ?
+        while self.pos < self.chars.len() && self.chars[self.pos] == '#' {
+            self.advance()
+        }
+
+        while self.pos < self.chars.len() && self.chars[self.pos] == ' ' {
+            self.advance()
+        }
+
+        let text_starts_at = self.pos;
+
+        while self.pos < self.chars.len() && self.chars[self.pos] != '\n' {
+            self.advance()
+        }
+
+        self.chars[text_starts_at..self.pos].iter().collect()
+    }
+
     pub fn scan(&mut self) {
         while !self.is_at_end() {
             println!("pos: {}, {}", self.pos, self.chars[self.pos]);
 
             if self.is_heading() {
-                println!("heading detected, {}, {}", self.pos, self.chars[self.pos])
-                // now extract into a Heading token
+                // println!("heading detected, {}, {}", self.pos, self.chars[self.pos]);
+                let heading = self.heading();
+                self.tokens.push(Token {
+                    content: heading,
+                    filename: String::from("unknown filename"),
+                    token_type: TokenType::Title,
+                });
+                println!("heading: {:?}", self.tokens);
             }
 
             // after all
             self.pos += 1;
         }
     }
-}
-
-fn is_new_line(char: char) -> bool {
-    char == '\n'
 }
