a00079b81b4bbe24ae3d1cc1a2d83022e6c0f5c7
Dragos Tudorache <hedphug@gmail.com>
2025-12-01 19:48:43 +0200

    add index document function

--- src/reverse_index.zig
+++ src/reverse_index.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const posting = @import("reverse_index/posting.zig");
+const markdown = @import("markdown/parser.zig");
+const recipeExtractor = @import("recipe_extractor.zig");
 
 pub const ReverseIndex = struct {
     allocator: std.mem.Allocator,
@@ -20,7 +22,7 @@ pub const ReverseIndex = struct {
         self.index.deinit();
     }
 
-    pub fn addPosting(self: *ReverseIndex, term: []const u8, item: posting.Posting) !void {
+    fn addPosting(self: *ReverseIndex, term: []const u8, item: posting.Posting) !void {
         const gop = try self.index.getOrPut(term);
         if (!gop.found_existing) {
             // clone the term
@@ -30,23 +32,65 @@ pub const ReverseIndex = struct {
         try gop.value_ptr.append(item);
     }
 
-    pub fn indexDocument(self: *ReverseIndex) {
-        // TODO
+    pub fn indexDocument(self: *ReverseIndex, document: struct { data: recipeExtractor.RecipeData }) !void {
+        var terms_map = std.StringHashMap(posting.Posting).init(self.allocator);
+        defer terms_map.deinit();
+
+        try updatePosting(&terms_map, document.data.title, .title);
+        try updatePosting(&terms_map, document.data.tags, .tags);
+        try updatePosting(&terms_map, document.data.ingredients, .ingredients);
+
+        var it = terms_map.iterator();
+        while (it.next()) |entry| {
+            std.debug.print("key: {s}, value: {any}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        }
+    }
+
+    fn updatePosting(terms_map: *std.StringHashMap(posting.Posting), terms_list: std.ArrayList([]const u8), source_field: posting.SourceField) !void {
+        for (terms_list.items) |term| {
+            const gop = try terms_map.getOrPut(term);
+
+            if (!gop.found_existing) {
+                gop.key_ptr.* = term;
+                gop.value_ptr.* = posting.Posting{ .document_id = 0, .term_frequency = 0, .source_field = source_field };
+            }
+            gop.value_ptr.term_frequency = gop.value_ptr.term_frequency + 1;
+        }
     }
 };
 
 test "initial" {
     const allocator = std.testing.allocator;
 
+    const source =
+        \\# Scrambled Eggs
+        \\
+        \\## tags
+        \\breakfast, easy, eggs
+        \\
+        \\## ingredients
+        \\- eggs
+        \\- butter
+    ;
+
+    var parser = markdown.Parser.init(allocator, source);
+    defer parser.deinit();
+    const lines = try parser.parse();
+
+    var extractor = recipeExtractor.RecipeExtractor.init(allocator);
+    defer extractor.deinit();
+    const recipeLines = try extractor.extract(lines);
+
     var ri = ReverseIndex.init(allocator);
     defer ri.deinit();
+    try ri.indexDocument(.{ .data = recipeLines });
 
-    try ri.addPosting("pasta", .{ .document_id = 39, .term_frequency = 2, .source_field = posting.SourceField.title });
-    try ri.addPosting("pasta", .{ .document_id = 2, .term_frequency = 2, .source_field = posting.SourceField.ingredients });
+    // try ri.addPosting("pasta", .{ .document_id = 39, .term_frequency = 2, .source_field = posting.SourceField.title });
+    // try ri.addPosting("pasta", .{ .document_id = 2, .term_frequency = 2, .source_field = posting.SourceField.ingredients });
 
-    if (ri.index.get("pasta")) |postings| {
-        for (postings.items.items) |item| {
-            std.debug.print("found: {any}, weight: {d} \n", .{ item, item.source_field.getWeight() });
-        }
-    }
+    // if (ri.index.get("pasta")) |postings| {
+    //     for (postings.items.items) |item| {
+    //         std.debug.print("found: {any}, weight: {d} \n", .{ item, item.source_field.getWeight() });
+    //     }
+    // }
 }
